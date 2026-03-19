041a48a3b2a3eed4985c8f26c57978fbe6ccfb1a
Andre Leal <andre.leal1992@gmail.com>
2026-03-19 08:37:57 -0300

    deploy final

--- core/Router.php
+++ core/Router.php
@@ -5,13 +5,27 @@ class Router {
     private $routes = [];
 
     public function get($uri, $callback) {
-        $this->routes['GET'][$uri] = $callback;
+        $this->routes['GET'][$this->normalize($uri)] = $callback;
+    }
+
+    private function normalize($uri) {
+        // remove barra final (exceto raiz)
+        if ($uri !== '/') {
+            $uri = rtrim($uri, '/');
+        }
+        return $uri;
     }
 
     public function dispatch() {
 
         $method = $_SERVER['REQUEST_METHOD'];
+
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        // remove /index.php da URL
+        $uri = str_replace('/index.php', '', $uri);
+
+        $uri = $this->normalize($uri);
 
         if(isset($this->routes[$method][$uri])) {
             call_user_func($this->routes[$method][$uri]);
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once __DIR__ . '/../core/Router.php';
+require_once __DIR__ . '/core/Router.php';
 
 $router = new Router();
 
 // Rotas
 $router->get('/', function() {
-    echo "NeoFleet SaaS ONLINE gora ja era🚀";
+    echo "NeoFleet SaaS ONLINE agora já era 🚀";
 });
 
 $router->dispatch();
