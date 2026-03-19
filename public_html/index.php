2933e672a875cc67c3cd3b0606b0431a69a6de31
Andre Leal <andre.leal1992@gmail.com>
2026-03-19 10:50:43 -0300

    update

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,16 @@
 
 require_once __DIR__ . '/../core/Router.php';
 
+// AJUSTE CRÍTICO
+$uri = $_SERVER['REQUEST_URI'];
+$uri = str_replace('/public_html', '', $uri);
+
+if ($uri === '') {
+    $uri = '/';
+}
+
+$_SERVER['REQUEST_URI'] = $uri;
+
 $router = new Router();
 
 $router->get('/', function() {
