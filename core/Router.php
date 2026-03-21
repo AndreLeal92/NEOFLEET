aba0b5af5d541c06b6774caf84e57473b71bf802
Andre Leal <andre.leal1992@gmail.com>
2026-03-21 15:53:51 -0300

    update

---
+++ core/Router.php
@@ -0,0 +1,31 @@
+<?php
+
+class Router {
+
+    private $routes = [];
+
+    public function get($uri, $callback) {
+        $this->routes['GET'][$uri] = $callback;
+    }
+
+    public function post($uri, $callback) {
+        $this->routes['POST'][$uri] = $callback;
+    }
+
+    public function dispatch($requestUri) {
+
+        $uri = parse_url($requestUri, PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        // remove barra final
+        $uri = rtrim($uri, '/') ?: '/';
+
+        if (isset($this->routes[$method][$uri])) {
+            call_user_func($this->routes[$method][$uri]);
+            return;
+        }
+
+        http_response_code(404);
+        echo "404 - Rota não encontrada";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+$router->get('/', function() {
+    require BASE_PATH . '/public_html/login.php';
+});
+
+$router->post('/login', function() {
+
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if ($email === 'admin@teste.com' && $password === '123456') {
+        $_SESSION['user'] = $email;
+        header('Location: /dashboard');
+        exit;
+    }
+
+    header('Location: /?error=1');
+    exit;
+});
+
+$router->get('/dashboard', function() {
+    echo "LOGADO COM SUCESSO 🚀";
+});
